dfb783a5bf148e3e002bcfa3669f72ea0a37bad3
SudonNoodle <SudonNoodle@derps.local>
2025-05-21 20:21:20 -0400

    Got basic image control working.  Need to move on to generated images.

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,8 @@
 using Avalonia.Controls;
+using Avalonia.Media.Imaging;
+using System;
+using Avalonia;
+using Avalonia.Reactive;
 
 namespace slime;
 
@@ -7,5 +11,22 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        Opened += OnOpened;
+        
     }
+
+    private void OnOpened(object? sender, EventArgs e)
+    {
+        Bitmap source = new Bitmap("/home/bo/Projects/slime/ffxiv_05192025_151441_926.png");
+        Image.Width = Math.Clamp(source.Size.Width, 0, ClientSize.Width);
+        Image.Height = Math.Clamp(source.Size.Height, 0, ClientSize.Height);
+        Image.Source = source;
+
+        this.GetObservable(ClientSizeProperty).Subscribe( new AnonymousObserver<Size>(sz =>
+        {
+            Image.Width = Math.Clamp(source.Size.Width, 0, sz.Width);
+            Image.Height = Math.Clamp(source.Size.Height, 0, sz.Height);
+        }));
+    }
+    
 }
